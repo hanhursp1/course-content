4f759bcf3bb67026fffc055470d4cba15e4910fa
hanhursp1 <hanhursp1@newpaltz.edu>
2024-02-26 15:14:38 -0500

    Added viewbar, changed bug-facts to true

--- client/src/views/TestView.vue
+++ client/src/views/TestView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="what">
-    <img class="bug-facts" src="@/assets/bug_facts.jpg">
+    <img class="bug-facts" src="@/assets/true.jpg">
   </div>
 </template>
 
@@ -9,11 +9,17 @@
   width: 50%;
   height: 50%;
   border-radius: 8px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
 }
 
 .what {
   border-radius: 8px;
   padding: 8px;
+  margin-left: auto;
+  display:block;
+  margin-right: auto;
   width: auto;
   border: 2px solid grey;
 }
