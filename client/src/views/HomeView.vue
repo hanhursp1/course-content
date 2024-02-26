5794987c2516228977785ee5d45a66895ee115c1
hanhursp1 <hanhursp1@newpaltz.edu>
2024-02-26 14:38:31 -0500

    Cleared out boilerplate

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="hero is-primary is-large">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">Home</h1>
+        <h2 class="subtitle">Welcome to the homepage!</h2>
+      </div>
+    </div>
   </main>
 </template>
---
+++ client/src/views/TestView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="what">
+    <img class="bug-facts" src="@/assets/bug_facts.jpg">
+  </div>
+</template>
+
+<style>
+.what .bug-facts {
+  width: 50%;
+  height: 50%;
+  border-radius: 8px;
+}
+
+.what {
+  border-radius: 8px;
+  padding: 8px;
+  width: auto;
+  border: 2px solid grey;
+}
+
+</style>
