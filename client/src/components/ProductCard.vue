5897a246546da1ad3624aebe5e0befc441fadc02
hanhursp1 <hanhursp1@newpaltz.edu>
2024-03-11 14:39:46 -0400

    Added product card

---
+++ client/src/components/ProductCard.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { Product } from '@/model/products';
+import { Ref, ref } from 'vue';
+
+const props = defineProps<{
+  product: Product
+}>()
+
+const emit = defineEmits<{
+  addToCart: [product: Product, qty: number]
+}>()
+
+let qty = ref(0)
+
+</script>
+
+<template>
+  <div class="card">
+    <div class="card-image">
+      <img :src="product.thumbnail" :alt="product.title">
+    </div>
+    <div class="card-content">
+      <p class="price">${{ product.price }}</p>
+      <h1>{{ product.title }}</h1>
+      <i>{{ product.brand }}</i>
+      <p>{{ product.description }}</p>
+      <button @click="emit('addToCart', product, qty)" class="button is-primary">Add to Cart</button>
+      <br>
+      <input type="number" v-model="qty" placeholder="Quantity">
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .card {
+    flex-basis: 18rem;
+  }
+
+  h3 {
+    font-weight: bold;
+  }
+
+  h1 {
+    font-size: xx-large;
+  }
+
+  .price {
+    font-weight: bold;
+    font-size: xx-large;
+    color: rgb(204, 64, 158);
+  }
+</style>
--- client/src/pages/products/index.vue
+++ client/src/pages/products/index.vue
@@ -7,6 +7,8 @@
   } from 'vue'
 
   import { type Product, getProducts } from "@/model/products"
+  import ProductCard from '@/components/ProductCard.vue';
+import FlyOut from '@/components/FlyOut.vue';
 
   const products = ref([] as Product[])
 
@@ -14,8 +16,6 @@
     product: Product,
     quantity: number
   }
-
-  const selectedQty = ref([] as number[])
   // selectedQty.value = (new Array<number>(products.value.length)).map(_ => 1)
 
   const cart = ref([] as CartItem[])
@@ -47,34 +47,20 @@
 
 <template>
   <div class="product-list">
-    <div v-for="product in products" :key="product.id" class="card">
-      <div class="card-image">
-        <img :src="product.thumbnail" :alt="product.title">
-      </div>
-      <div class="card-content">
-        <p class="price">${{ product.price }}</p>
-        <h1>{{ product.title }}</h1>
-        <i>{{ product.brand }}</i>
-        <p>{{ product.description }}</p>
-        <button @click="addWithQuantity(product, selectedQty[product.id - 1])" class="button is-primary">Add to Cart</button>
-        <br>
-        <input type="number" v-model="selectedQty[product.id - 1]" placeholder="Quantity">
-      </div>
-    </div>
-    <div class="flyout">
-      <h1 class="cart">
-        Cart
-      </h1>
-      <ul class="cart">
-        <li v-for="item in cart" :key="item.product.id">
-          <img :src="item.product.thumbnail" :alt="item.product.title">
-          {{ item.product.title }} x {{ item.quantity }} = $ {{ item.product.price * item.quantity }}
-        </li>
-        {{ cart.length }} items totalling ${{ total.toFixed(2) }}
-      </ul>
-      
-    </div>
+    <ProductCard v-for="product in products" :key="product.id" :product="product" @add-to-cart="addWithQuantity" />
   </div>
+  <FlyOut :is-open="true">
+    <h1 class="cart">
+      Cart
+    </h1>
+    <ul class="cart">
+      <li v-for="item in cart" :key="item.product.id">
+        <img :src="item.product.thumbnail" :alt="item.product.title">
+        {{ item.product.title }} x {{ item.quantity }} = $ {{ item.product.price * item.quantity }}
+      </li>
+      {{ cart.length }} items totalling ${{ total.toFixed(2) }}
+    </ul>
+  </FlyOut>
 </template>
 
 <style scoped>
@@ -83,42 +69,6 @@
     display: flex;
     flex-wrap: wrap;
     justify-content: space-around;
-  }
-  .card {
-    flex-basis: 18rem;
-  }
-
-  h3 {
-    font-weight: bold;
-  }
-
-  h1 {
-    font-size: xx-large;
-  }
-
-  .price {
-    font-weight: bold;
-    font-size: xx-large;
-    color: rgb(204, 64, 158);
-  }
-
-  .flyout {
-    position: fixed;
-    top: 0;
-    right: 0;
-    width: 20rem;
-    height: 100%;
-    background-color: white;
-    border: 1px solid #ccc;
-    box-shadow: -1px 0 5px 0 black;
-    z-index: 100;
-    transform: translateX(80%);
-    transition: transform .3s;
-    padding: 1rem;
-  }
-
-  .flyout.open, .flyout:hover {
-    transform: translateX(0);
   }
 
   .cart {
